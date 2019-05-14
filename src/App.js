380372ed3f952f54ed0bcb2e94c8d003da31d6d9
louis <louis@louisde-MacBook-Pro.local>
2019-05-15 02:35:51 +0800

    runkit gen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import * as monaco from 'monaco-editor';
+import * as _ from 'lodash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: ''
+    }
+  }
+
+  componentDidMount() {
+    if (!this.editor2) {
+      let editor = monaco.editor.create(document.getElementById("editor"), {
+        language: "javascript",
+        theme: "vs-dark"
+      });
+      editor.onDidChangeModelContent(
+        _.debounce(() => {
+          let code = editor.getValue()
+          let base64code = encodeURIComponent(Buffer.from(code).toString('base64'))
+          let iframe = `<iframe src="https://runkit.com/e?base64source=${base64code}" frameborder="0" style="width: 100%;"></iframe>`;
+          this.editor2.setValue(iframe);
+          this.setState({ code: iframe })
+        }, 2000)
+      )
+
+      this.editor2 = monaco.editor.create(document.getElementById("editor2"), {
+        language: "html",
+        theme: "vs-dark",
+      });
+
+      this.editor2.onDidChangeModelContent(
+        _.debounce(() => {
+          let code = this.editor2.getValue()
+          this.setState({ code })
+        },2000)
+      )
+
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div id="editor" key="editor"
+          style={{ width: '100%', height: '200px' }}
+        />
+        <div id="editor2" key="editor2"
+          style={{ width: '100%', height: '100px' }}
+        />
+        <div dangerouslySetInnerHTML={{ __html: this.state.code }} />
+      </Fragment>
+    );
+  }
 }
 
-export default App;
+export default App
